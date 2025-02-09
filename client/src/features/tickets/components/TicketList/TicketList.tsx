f45cd1ef0cc2810c91bf52320e363862062c4cea
Viktor.Kondolovskyi <viktor.kondolovskyi@tui.com>
2025-02-09 17:56:14 +0200

    update styles

--- client/src/features/tickets/components/TicketList/TicketList.tsx
+++ client/src/features/tickets/components/TicketList/TicketList.tsx
@@ -39,7 +39,7 @@ export const TicketList = ({ userType, search }: { userType: string, search: str
     }
 
     return (
-        <div>
+        <div className='max-w-6xl mx-auto p-4'>
             <div className={userType === 'local' ? 'grid grid-cols-2 gap-4 h-full' : 'flex flex-col gap-4'}>
                 {tickets.map((ticket: Ticket) => (
                     <Card key={ticket.id}>
